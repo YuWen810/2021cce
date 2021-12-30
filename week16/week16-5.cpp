e36edc75023333339df8468a1e3217f0ee426902
ChangYuWen <81633513+YuWen810@users.noreply.github.com>
2021-12-30 17:40:57 +0800

    Add files via upload

---
+++ week16/week16-5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+char line[300]="0101011010100101011101";
+int main()
+{
+    int ans=0;
+    int n=strlen(line);
+    for(int i=0;i<n;i++)
+    {
+        if(line[i]=='1')
+            ans++;
+    }
+    printf("¦³%d­Ó1",ans);
+}
